d06b6f668424c7c857b68534fe99960bf88bff91
eagirrezabal <ekainaguirrezabal@gmail.com>
2021-01-03 13:58:43 +0100

    separate db configuration from config file

--- _system/_config.inc.php
+++ _system/_config.inc.php
@@ -6,18 +6,17 @@ ini_set('display_errors', 'on');
 date_default_timezone_set('Europe/Paris');
 
 // Constants
-define('AUTHOR', 'Jean-Charles Riquet');
+define('AUTHOR', 'Ekain Agirrezabal');
 define('TAB', chr(9));
 define('RET', ' ' . chr(10) . chr(13));
 define('NL', chr(13));
 define('DOCUMENT_ROOT', dirname(__FILE__) . '/../');
-define('CLASSGENERATOR_DIR', DOCUMENT_ROOT . '_class/');
+//define('CLASSGENERATOR_DIR', DOCUMENT_ROOT . '_class/');
+define('CLASSGENERATOR_DIR', DOCUMENT_ROOT . 'Entities/');
 
-define('dbhostname', 'localhost');
-define('dbdatabase', 'library');
-define('dbusername', 'root');
-define('dbpassword', '');
-define('dbtype', 'mysql');
+// Database credentials
+include '_db_config.inc.php';
+
 
 include '_database.class.php';
 include '_generator.class.php';
